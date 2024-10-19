be8f202ad184737242e31b8c051f8465dddab2b2
Sumire Doi <m245461@hiroshima-u.ac.jp>
2024-10-19 16:59:05 +0900

    index.htmlとapp.pyの修正 ファイル構造を変更

--- app.py
+++ app.py
@@ -1,11 +1,42 @@
-from flask import Flask, jsonify, request, render_template
+from flask import Flask, render_template, jsonify, request, redirect
+
 app = Flask(__name__)
+
+# ピン情報を格納するリスト
+pins = []
 
 # APIエンドポイントの定義
 @app.route('/api/data', methods=['GET'])
 def get_data():
     data = {"message": "Hello from Python!"}
     return jsonify(data)
+
+# 地図表示ページのエンドポイント
+@app.route('/', methods=['GET'])
+def index():
+    return render_template('index.html', pins=pins)
+
+# 新しいピンを追加するページのエンドポイント
+@app.route('/event_reg', methods=['GET', 'POST'])
+def event_reg():
+
+    # まだ仮
+    if request.method == 'POST':
+        # フォームからピンの情報を取得
+        name = request.form['name']
+        latitude = float(request.form['latitude'])
+        longitude = float(request.form['longitude'])
+
+        # ピン情報をリストに追加
+        pins.append({
+            'name': name,
+            'latitude': latitude,
+            'longitude': longitude
+        })
+
+        # 地図ページにリダイレクト
+        return redirect('/')
+    return render_template('event_reg.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
